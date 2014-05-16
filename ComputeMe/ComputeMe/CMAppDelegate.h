2f2407b4092cf8c15a715ab4f807c2c4c6719d93
Kok Hong <kokhong200@gmail.com>
2014-05-16 18:50:14 +0800

    #update - Init ComputeMe project

---
+++ ComputeMe/ComputeMe/CMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMAppDelegate.h
+//  ComputeMe
+//
+//  Created by Kok Hong on 5/16/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ComputeMe/ComputeMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ComputeMe
+//
+//  Created by Kok Hong on 5/16/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMAppDelegate class]));
+    }
+}
