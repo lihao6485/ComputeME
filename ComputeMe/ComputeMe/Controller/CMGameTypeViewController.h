b5c1cdfc7fd665cff1bf7b1ac443577f10513395
LiHao <lihao6485@gmail.com>
2014-05-19 11:22:18 +0800

    [update] - create two storyboard: initial and question; two view controller: start and select game type; change initial view to start;

---
+++ ComputeMe/ComputeMe/Controller/CMGameTypeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMGameTypeViewController.h
+//  ComputeMe
+//
+//  Created by Lai Li Hao on 5/19/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMGameTypeViewController : UIViewController
+
+@end
---
+++ ComputeMe/ComputeMe/Controller/CMStartViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMStartViewController.h
+//  ComputeMe
+//
+//  Created by Lai Li Hao on 5/19/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMStartViewController : UIViewController
+
+@end
