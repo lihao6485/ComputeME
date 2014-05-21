dacb015f6967226f85808e2d4b125c4bdbae55f3
LiHao <lihao6485@gmail.com>
2014-05-21 20:24:52 +0800

    @update - add question table view controller and temporality app icon

---
+++ ComputeMe/ComputeMe/Controller/CMQuestionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMQuestionTableViewController.h
+//  ComputeMe
+//
+//  Created by Lai Li Hao on 5/21/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMQuestionTableViewController : UITableViewController
+
+@end
