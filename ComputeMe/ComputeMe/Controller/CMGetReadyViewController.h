2caba0ccfde86e60acd1d54ab24834a0927d5114
Kok Hong <kokhong200@gmail.com>
2014-05-25 17:54:26 +0800

    @update - Get ready page done.

---
+++ ComputeMe/ComputeMe/Controller/CMGetReadyViewController.h
@@ -0,0 +1,15 @@
+//
+//  CMGetReadyViewController.h
+//  ComputeMe
+//
+//  Created by Kok Hong on 5/25/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMGetReadyViewController : UIViewController 
+@property (weak, nonatomic) IBOutlet UILabel *getReadyLabel;
+@property (weak, nonatomic) IBOutlet UILabel *countDownLabel;
+
+@end
