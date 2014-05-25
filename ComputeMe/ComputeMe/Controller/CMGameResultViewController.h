1556657c1c2079d73da855df74e14140f7a1e4c6
Kok Hong <kokhong200@gmail.com>
2014-05-25 19:13:08 +0800

    @update - Game result page done. Temporary with dummy data inserted.

---
+++ ComputeMe/ComputeMe/Controller/CMGameResultViewController.h
@@ -0,0 +1,16 @@
+//
+//  CMGameResultViewController.h
+//  ComputeMe
+//
+//  Created by Kok Hong on 5/25/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CMGameResultViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *gameModeLabel;
+@property (copy, nonatomic) NSDictionary *gameDetails;
+@property (weak, nonatomic) IBOutlet UILabel *correctAnswerLabel;
+
+@end
