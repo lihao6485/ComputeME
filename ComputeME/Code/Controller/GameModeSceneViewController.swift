0afb34ef6255a1cd7d402d5c319c501762137ffe
Li Hao <LiHao@Lais-MacBook-Pro.local>
2014-08-19 18:42:55 +0800

    #update - Select game mode scene is created
    Select game mode scene created and link with start scene

---
+++ ComputeME/Code/Controller/GameModeSceneViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameModeSceneViewController.swift
+//  ComputeME
+//
+//  Created by Li Hao on 8/19/14.
+//  Copyright (c) 2014 Pointwelve. All rights reserved.
+//
+
+import UIKit
+
+class GameModeSceneViewController: UIViewController {
+    
+    @IBOutlet var storyButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- ComputeME/Code/Controller/StartSceneViewController.swift
+++ ComputeME/Code/Controller/StartSceneViewController.swift
@@ -15,14 +15,18 @@ class StartSceneViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.Repeat|UIViewAnimationOptions.CurveEaseIn|UIViewAnimationOptions.Autoreverse|UIViewAnimationOptions.AllowUserInteraction,
+        self.navigationController.navigationBarHidden = true
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.Repeat|UIViewAnimationOptions.Autoreverse,
             animations: { () -> Void in
-            self.startLabel.alpha = 0.0
-            println("bello");
-        }, completion: nil)
+            self.startLabel.alpha = 0.0}, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func handleTap(recognizer: UITapGestureRecognizer) {
+        let gameModeSceneViewController: GameModeSceneViewController = self.storyboard.instantiateViewControllerWithIdentifier("GameModeSelectionScene") as GameModeSceneViewController
+        self.navigationController.pushViewController(gameModeSceneViewController, animated: true)
     }
 }
